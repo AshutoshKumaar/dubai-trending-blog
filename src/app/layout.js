9929a80d6f78bee2973d42cc6cead744104f09ff
AshutoshKumaar <sinhapintu545@gmail.com>
2026-03-14 23:51:24 +0530

    Lyout-chnges

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist_Mono, Mooli } from "next/font/google";
 import "./globals.css";
 import SiteFooter from "@/components/SiteFooter";
 import SiteNavbar from "@/components/SiteNavbar";
+import Script from "next/script";
 
 const mooli = Mooli({
   variable: "--font-mooli",
@@ -15,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  metadataBase: new URL("https://dubaitrendingblog.com"),
+  metadataBase: new URL("https://dubaitrendings.com"),
   title: {
     default: "Dubai Trending Blog | Local Insights, Lifestyle, and Business Updates",
     template: "%s | Dubai Trending Blog",
@@ -29,7 +30,7 @@ export const metadata = {
     title: "Dubai Trending Blog",
     description:
       "Fresh and practical Dubai insights covering lifestyle, tourism, local business, and trends.",
-    url: "https://dubaitrendingblog.com",
+    url: "https://dubaitrendings.com",
     siteName: "Dubai Trending Blog",
     locale: "en_US",
     type: "website",
@@ -39,6 +40,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3336554213083495"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${mooli.variable} ${geistMono.variable} antialiased`}>
         <div>
           <SiteNavbar />
